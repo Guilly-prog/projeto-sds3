2d75084b80ee3c10130d264d8f095ac9cd983607
Guilherme Lopes <glopes865@gmail.com>
2021-05-04 23:55:33 -0300

    Basic static components

---
+++ frontend/src/components/DataTable/index.tsx
@@ -0,0 +1,91 @@
+const DataTable = () => {
+    return (
+        <div className="table-responsive">
+            <table className="table table-striped table-sm">
+                <thead>
+                    <tr>
+                        <th>Data</th>
+                        <th>Vendedor</th>
+                        <th>Clientes visitados</th>
+                        <th>Negócios fechados</th>
+                        <th>Valor</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Logan</td>
+                        <td>15</td>
+                        <td>12</td>
+                        <td>15017.00</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Barry Allen</td>
+                        <td>20</td>
+                        <td>17</td>
+                        <td>20050.00</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Dovahkiin</td>
+                        <td>40</td>
+                        <td>39</td>
+                        <td>45200.00</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Barry Allen</td>
+                        <td>9</td>
+                        <td>5</td>
+                        <td>9360.00</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Peter Parker</td>
+                        <td>7</td>
+                        <td>7</td>
+                        <td>7420.00</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Barry Allen</td>
+                        <td>16</td>
+                        <td>12</td>
+                        <td>16523.00</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Dovahkiin</td>
+                        <td>21</td>
+                        <td>15</td>
+                        <td>21780.00</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Barry Allen</td>
+                        <td>31</td>
+                        <td>28</td>
+                        <td>33598.00</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Barry Allen</td>
+                        <td>29</td>
+                        <td>21</td>
+                        <td>15017.00</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Bruce Wayne</td>
+                        <td>28</td>
+                        <td>26</td>
+                        <td>999999.00</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default DataTable;
